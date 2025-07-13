de21aeb6eac75c35f2de2b972255e48749046ad1
404nicolette <123152389+404nicolette@users.noreply.github.com>
2025-07-13 18:16:33 +0100

    fixed the spacing on pagethree

--- src/page/PageThree.jsx
+++ src/page/PageThree.jsx
@@ -5,7 +5,7 @@ function PageThree() {
     const [nextImage, setNextImage]=useState(false)
         
     return (
-        <div className=' h-screen w-screen flex flex-col items-center justify-start px-8 md:h-screen space-y-10 slide-in'>
+        <div className=' h-screen w-screen flex flex-col items-center justify-start px-8 md:h-screen space-y-5 slide-in'>
             <h1 className='py-5 text-center font-forghive text-[#6B0000] text-[3.5rem] underline decoration-[1px]'>style inspo</h1>
 
             <div className='border-1  overflow-hidden'>
@@ -17,7 +17,7 @@ function PageThree() {
             </div>
 
            <div onClick={()=> setNextImage(!nextImage)} className='flex items-center justify-center border rounded-full w-14 h-14 border-[#6B0000] my-5 cursor-pointer'>
-                <h5 className='font-n text-[#6B0000] p-2'>N</h5>
+                <h5 className='font-bold font-n text-[#6B0000] p-2'>N</h5>
             </div>
         </div>
     )
